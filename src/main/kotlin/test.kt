870cb753d6952a16a0ec0c0479e0498b055893eb
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-06-02 16:26:40 +0300

    add kotlin support

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,3 +1,25 @@
+import java.util.Spliterator
+import java.util.function.Consumer
+
 fun main() {
     println("Hello World!")
+}
+class Spliterator : Spliterator<String> {
+
+    override fun tryAdvance(action: Consumer<in String>?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun trySplit(): Spliterator<String?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun estimateSize(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun characteristics(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
