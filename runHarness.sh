edc2488595903f78829849100551eb3004bd406f
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-06-03 10:12:34 +0300

    Update benchmark configurations and script for improved performance

--- runHarness.sh
+++ runHarness.sh
@@ -1,3 +1,5 @@
 ./gradlew shadowJar
-java -jar build/libs/benchmarks.jar SimpleHarnessBenchmark
+java -jar build/libs/benchmarks.jar SimpleHarnessBenchmark \
+  -rf json -rff results.json
+cat results.json | jq '.[] | {name: .benchmark, score: .primaryMetric.score, unit: .primaryMetric.scoreUnit}'
 #java -jar build/libs/benchmarks.jar StreamParallelBenchmargk
