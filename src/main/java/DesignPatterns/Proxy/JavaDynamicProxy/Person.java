e326e98fb92f35e4bb962efefa2c2024ce7dd4c4
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-06-17 05:11:18 +0300

    Add Proxy pattern implementation with MatchMakingTestDrive, OwnerInvocationHandler, NonOwnerInvocationHandler, Person, and PersonImpl classes

---
+++ src/main/java/DesignPatterns/Proxy/JavaDynamicProxy/Person.java
@@ -0,0 +1,21 @@
+package DesignPatterns.Proxy.JavaDynamicProxy;
+
+public interface Person {
+
+    String getName();
+
+    void setName(String name);
+
+    String getGender();
+
+    void setGender(String gender);
+
+    String getInterests();
+
+    void setInterests(String interests);
+
+    int getGeekRating();
+
+    void setGeekRating(int rating);
+
+}
